a20cf9611a3dba2770f4e2bd3d7504739fd5bcaa
Dmytro Skrypka <2346677tyu@gmail.com>
2022-11-14 12:44:48 +0200

    Update tag_list and sidebar

--- todo/views.py
+++ todo/views.py
@@ -1,4 +1,3 @@
-from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views import generic
@@ -6,41 +5,41 @@ from django.views import generic
 from todo.models import Tag, Task
 
 
-class TagListView(LoginRequiredMixin, generic.ListView):
+class TagListView(generic.ListView):
     model = Tag
     paginate_by = 5
 
 
-class TagCreateView(LoginRequiredMixin, generic.CreateView):
+class TagCreateView(generic.CreateView):
     model = Tag
     fields = "__all__"
     success_url = reverse_lazy("todo:tag-list")
 
 
-class TagUpdateView(LoginRequiredMixin, generic.UpdateView):
+class TagUpdateView(generic.UpdateView):
     model = Tag
     fields = "__all__"
     success_url = reverse_lazy("todo:tag-list")
 
 
-class TagDeleteView(LoginRequiredMixin, generic.DeleteView):
+class TagDeleteView(generic.DeleteView):
     model = Tag
     success_url = reverse_lazy("togo:tag-list")
     template_name = "todo/tag_confirm_delete.html"
 
 
-class TaskListView(LoginRequiredMixin, generic.ListView):
+class TaskListView(generic.ListView):
     model = Task
     paginate_by = 5
 
 
-class TaskCreateView(LoginRequiredMixin, generic.CreateView):
+class TaskCreateView(generic.CreateView):
     model = Task
     fields = "__all__"
     success_url = reverse_lazy("togo:task-list")
 
 
-class TaskUpdateView(LoginRequiredMixin, generic.UpdateView):
+class TaskUpdateView(generic.UpdateView):
     model = Task
     fields = "__all__"
     success_url = reverse_lazy("togo:task-list")
