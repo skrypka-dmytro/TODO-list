09ab1e020b51a3a1aa8cea217e2b703e78123c1a
Dmytro Skrypka <2346677tyu@gmail.com>
2022-11-14 11:06:20 +0200

    Added usrl.py path: tegs/create/ and create TagCreateView

--- todo/urls.py
+++ todo/urls.py
@@ -1,11 +1,16 @@
 from django.urls import path
 
-from todo.views import TagListView
+from todo.views import TagListView, TagCreateView
 
 urlpatterns = [
     path(
         "tags/",
         TagListView.as_view(),
         name="tag-list"
+    ),
+    path(
+        "tags/crate?",
+        TagCreateView.as_view(),
+        name="tag-create"
     ),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views import generic
 
 from todo.models import Tag
@@ -7,3 +8,9 @@ from todo.models import Tag
 
 class TagListView(LoginRequiredMixin, generic.ListView):
     model = Tag
+
+
+class TagCreateView(LoginRequiredMixin, generic.CreateView):
+    model = Tag
+    fields = "__all__"
+    success_url = reverse_lazy("todo:tag-list")
